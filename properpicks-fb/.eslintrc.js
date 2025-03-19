d8a862977bc204d9a9b7e9ff504625c73228db24
Papa-Sean <smsugrue@gmail.com>
2025-03-19 19:39:38 -0400

    Refactor ESLint configurations for consistency; streamline Firebase predeploy scripts; remove unused public/index.html

--- properpicks-fb/.eslintrc.js
+++ properpicks-fb/.eslintrc.js
@@ -1,28 +1,27 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-  },
-  parserOptions: {
-    "ecmaVersion": 2018,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
-  },
-  overrides: [
-    {
-      files: ["**/*.spec.*"],
-      env: {
-        mocha: true,
-      },
-      rules: {},
-    },
-  ],
-  globals: {},
+	root: true, // Important - tells ESLint to stop here
+	env: {
+		es6: true,
+		node: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+	},
+	extends: ['eslint:recommended', 'google'],
+	rules: {
+		'no-restricted-globals': ['error', 'name', 'length'],
+		'prefer-arrow-callback': 'error',
+		quotes: ['error', 'double', { allowTemplateLiterals: true }],
+	},
+	ignorePatterns: ['node_modules/**'],
+	overrides: [
+		{
+			files: ['**/*.spec.*'],
+			env: {
+				mocha: true,
+			},
+			rules: {},
+		},
+	],
+	globals: {},
 };
